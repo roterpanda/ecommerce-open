bf05d2f9528492098dcb79bc61f5dc8f60208c79
roterpanda <sascha@brandt-digital.de>
2020-05-29 11:05:22 +0200

    basic routing setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+function HatsPage() {
+  return <h2>Hats</h2>;
+}
+
+
+
 function App() {
   return (
     <div>
-      <HomePage />
+
+      <Switch>
+        <Route path='/' exact component={HomePage}>
+
+        </Route>
+        <Route path='/shop/hats' component={HatsPage}>
+
+        </Route>
+      </Switch>
+
+
     </div>
   );
 }
