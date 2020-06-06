dbc76163d3c8c174bb399c27ebdf2727c64e4852
roterpanda <sascha@brandt-digital.de>
2020-06-06 16:22:51 +0200

    adding sign up form

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -30,7 +30,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
                 email,
                 createdAt,
                 ...additionalData
-            })
+            });
         } catch (error) {
             console.log('error creating user', error.message);
         }
